47fa59293be514ee785e4c5a85e62f936e688bfb
Arman-shiyam <arman111hasan@gmail.com>
2024-02-21 23:14:40 +0600

    added recover file in github

---
+++ event.js
@@ -0,0 +1,8 @@
+// console.log('This is separate JS file');
+
+// document.getElementById('make-goldenrod').addEventListener('click', function () {
+//     document.body.style.backgroundColor = 'goldenrod';
+// })
+
+const nameOfOrganization = document.getElementById("name").value;
+console.log(nameOfOrganization);
